57df8e5a0882d608e0719b7bd94d23f41b9d8b91
Владимир <nickfaces@gmail.com>
2023-01-13 18:23:14 +0500

    1

--- libs/uix/baseclass/home_screen.py
+++ libs/uix/baseclass/home_screen.py
@@ -1,19 +1,29 @@
 from kivy.uix.screenmanager import Screen
 import pandas as pd
+from plyer import filechooser
 
 class HomeScreen(Screen):
-
-    # changing screens also can be done in python
-    # def goto_settings_screen(self):
-    #     self.manager.set_current("settings")
     pass
 
-    def check_connection(self):
+    def check_file(self):
         # input_file = pd.read_excel('Клиенты. Пример импорта.xlsx')
         current_path = self.ids.input_file.text
-        input_file = pd.read_excel(current_path)
+        try:
+            input_file = pd.read_excel(current_path)
 
-        out = input_file.columns.values.tolist()
-        print('ok ok ok')
-        print(current_path)
-        print(out)
+            out = input_file.columns.values.tolist()
+            print('ok ok ok')
+            print(current_path)
+            print(out)
+            self.goto_settings_screen()
+        except Exception as e:
+            print(e)
+
+
+    # changing screens also can be done in python
+    def goto_settings_screen(self):
+        self.manager.set_current("settings")
+
+    def filechoose(self):
+        path = filechooser.open_file()[0]
+        self.ids.input_file.text = path
--- libs/uix/baseclass/settings_screen.py
+++ libs/uix/baseclass/settings_screen.py
@@ -1,5 +1,7 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.list import OneLineListItem
 
 class SettingsScreen(Screen):
 
@@ -7,3 +9,23 @@ class SettingsScreen(Screen):
     # def goto_home_screen(self):
     #     self.manager.goback()
     pass
+
+    def drop(self):
+
+
+        items = [
+            {
+                "viewclass": "OneLineListItem",
+                "height": dp(56),
+                "text": f"Item {i}",
+            }
+            for i in range(5)
+        ]
+        self.menu = MDDropdownMenu(
+            caller=self.ids.drop_item,
+            items=items,
+            position="auto",
+            width_mult=4,
+        )
+        self.menu.open()
+
